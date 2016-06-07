b60db8c62fd5ff2186f8b9d064c88e8ec0a09b29
hxs2660 <hxs2660@163.com>
2016-06-07 10:36:51 +0800

    modified:   functional_tests.py 	ghostdriver.log

--- functional_tests.py
+++ functional_tests.py
@@ -1,9 +1,26 @@
 #!/usr/bin/env python3
 #codeing:utf-8
+
+import unittest
 from selenium import webdriver
-import time
-browser=webdriver.PhantomJS(executable_path='./phantomjs')
-browser.get('http://localhost:8000')
-assert 'Django' in browser.title
-#print(browser.page_source)
-browser.close()
+
+class NewVisitorTest(unittest.TestCase):
+    """docstring for NewVisittorTest"""
+    #测试之前运行
+    def setUp(self):
+        self.browser=webdriver.PhantomJS(executable_path='../phantomjs')
+        self.browser.implicitly_wait(3)
+
+    #测试之后运行    
+    def tearDown(self):
+        self.browser.quit()
+
+    #测试方法    
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('To-DO',self.browser.title)
+        self.fail('Finish the test!')
+    
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
