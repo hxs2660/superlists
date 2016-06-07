440a46801f71fdf426464bd7f1a58273a779f47e
hxs2660 <hxs2660@163.com>
2016-06-07 22:51:34 +0800

    Model for list Items

--- functional_tests.py
+++ functional_tests.py
@@ -3,6 +3,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
+import time
 
 class NewVisitorTest(unittest.TestCase):
     """docstring for NewVisittorTest"""
@@ -15,6 +16,11 @@ class NewVisitorTest(unittest.TestCase):
     #测试之后运行    
     def tearDown(self):
         self.browser.quit()
+
+    def check_for_row_in_list_table(self,row_text):
+        table=self.browser.find_element_by_id('id_list_table')    
+        rows=table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text,[row.text for row in rows])
 
     #测试方法    
     def test_can_start_a_list_and_retrieve_it_later(self):
@@ -29,9 +35,17 @@ class NewVisitorTest(unittest.TestCase):
                         'Enter a to-do item')
         inputbox.send_keys('Buy peacock feathers')
         inputbox.send_keys(Keys.ENTER)
-        table=self.browser.find_element_by_id('id_list_table')
-        rows=table.find_elements_by_tag_name('tr')
-        self.assertTrue(any(row.text=='1:Buy peacock feathers' for row in rows))
+        self.browser.implicitly_wait(10)        
+        self.check_for_row_in_list_table('1:Buy peacock feathers')
+        #页面又显示了一个文本框，可以输入其他待办事项
+        inputbox=self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        self.browser.implicitly_wait(10)
+        self.check_for_row_in_list_table('1:Buy peacock feathers')
+        self.check_for_row_in_list_table('2:Use peacock feathers to make a fly')
+        #self.assertTrue(any(row.text=='1:Buy peacock feathers' for row in rows),
+        #                "New to-do item did not appear in table --its text was:\n{}".format(table.text))
 
         self.fail('Finish the test!')
     
