549b6188d460d744ffe143bf7ca68c2447b76463
hxs2660 <hxs2660@163.com>
2016-06-08 11:25:53 +0800

    new file:   functional_tests/__init__.py 	renamed:    functional_tests.py -> functional_tests/tests.py 	modified:   ghostdriver.log 	modified:   lists/templates/home.html 	modified:   lists/tests.py 	modified:   lists/views.py 	modified:   superlists/urls.py

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -2,10 +2,11 @@
 #codeing:utf-8
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import unittest
+#import unittest
 import time
+from django.test import LiveServerTestCase
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
     """docstring for NewVisittorTest"""
     #测试之前运行
     def setUp(self):
@@ -24,7 +25,7 @@ class NewVisitorTest(unittest.TestCase):
 
     #测试方法    
     def test_can_start_a_list_and_retrieve_it_later(self):
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
         #self.browser.get('http://blog.hehome.top')
         self.assertIn('To-Do',self.browser.title)
         header_text=self.browser.find_element_by_tag_name('h1').text
@@ -33,23 +34,45 @@ class NewVisitorTest(unittest.TestCase):
         self.assertEqual(
                         inputbox.get_attribute('placeholder'),
                         'Enter a to-do item')
+        #edith输入了一个新待办事项，新建了一个清单
         inputbox.send_keys('Buy peacock feathers')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(3)        
+        time.sleep(3)
+        #edith获得了他的唯一URL       
+        edith_list_url=self.browser.current_url
+        self.assertRegex(edith_list_url,'/lists/.+')        
         self.check_for_row_in_list_table('1:Buy peacock feathers')
         #页面又显示了一个文本框，可以输入其他待办事项
         inputbox=self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
         time.sleep(3)
-        #self.browser.implicitly_wait(10)
+        #检查两次输入清单是否已经保存
         self.check_for_row_in_list_table('1:Buy peacock feathers')
         self.check_for_row_in_list_table('2:Use peacock feathers to make a fly')
-        #self.assertTrue(any(row.text=='1:Buy peacock feathers' for row in rows),
-        #                "New to-do item did not appear in table --its text was:\n{}".format(table.text))
+        
+        #使用一个新浏览器会话
+        self.browser.quit()
+        self.setUp()
 
-        self.fail('Finish the test!')
+        #首页看不到上面的清单
+        self.browser.get(self.live_server_url)
+        page_text=self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers',page_text)
+        self.assertNotIn('make a fly',page_text)
+
+        #francis输入了一个新待办事项，新建了一个清单
+        inputbox=self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(3)
+
+        #francis获得了他的唯一URL
+        francis_list_url=self.browser.current_url
+        self.assertRegex(francis_list_url,'/lists/.+')
+        self.assertNotEqual(francis_list_url,edith_list_url)
+
+        print(francis_list_url,edith_list_url,sep=',')
+
+        #self.fail('Finish the test!')
     
-
-if __name__ == '__main__':
-    unittest.main(warnings='ignore')
--- lists/tests.py
+++ lists/tests.py
@@ -6,17 +6,18 @@ from django.template.loader import render_to_string
 from lists.models import Item
 
 class HomePageTest(TestCase):
+    #测试主页解析
     def test_root_url_resolve_to_home_page_view(self):
         found=resolve('/')
         self.assertEqual(found.func,home_page)
-    
+    #返回正确首页
     def test_home_page_returns_correct_html(self):
         request=HttpRequest()
         response=home_page(request)
         expected_html=render_to_string('home.html')
         #print(expected_html)
         self.assertEqual(response.content.decode(),expected_html)
-
+    #测试保存POST请求    
     def test_home_page_can_save_a_POST_request(self):
         request=HttpRequest()
         request.method='POST'
@@ -26,7 +27,7 @@ class HomePageTest(TestCase):
         self.assertEqual(Item.objects.count(),1)
         new_item=Item.objects.first()
         self.assertEqual(new_item.text,'A new list item')
-
+    #POST之后重定向    
     def test_home_page_redirects_after_POST(self):
         request=HttpRequest()
         request.method='POST'
@@ -34,20 +35,11 @@ class HomePageTest(TestCase):
 
         response=home_page(request)
         self.assertEqual(response.status_code,302)
-        self.assertEqual(response['location'],'/')
-
-    def test_home_page_displays_all_list_items(self):
-        Item.objects.create(text='itemey 1')
-        Item.objects.create(text='itemey 2')
-
-        request=HttpRequest()
-        response=home_page(request)
-
-        self.assertIn('itemey 1',response.content.decode())
-        self.assertIn('itemey 2',response.content.decode())
-
+        self.assertEqual(response['location'],'/lists/the-only-list-in-the-world/')
+       
 
 class ItemModelTest(TestCase):
+    #保存、读出
     def test_saving_and_retrieving_items(self):
         first_item=Item()
         first_item.text='The first (ever) list item'
@@ -64,3 +56,18 @@ class ItemModelTest(TestCase):
         second_saved_item=saved_items[1]
         self.assertEqual(first_saved_item.text,'The first (ever) list item')
         self.assertEqual(second_saved_item.text,'Item the second')
+
+class ListViewTest(TestCase):
+    #测试使用模版
+    def test_uses_list_Template(self):
+        response=self.client.get('/lists/the-only-list-in-the-world/')
+        self.assertTemplateUsed(response,'list.html')
+
+    #主页显示所有清单 
+    def test_displays_all_items(self):
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+        
+        response=self.client.get('/lists/the-only-list-in-the-world/')
+        self.assertContains(response,'itemey 1')
+        self.assertContains(response,'itemey 2')
